548eda12ad1c3e298f0b246bd01731701c59d7a2
Sashidhar <167504082+sashidhar-gloify@users.noreply.github.com>
2024-05-27 13:21:29 +0530

    Create Products.js

---
+++ Products.js
@@ -0,0 +1,178 @@
+[
+  {
+    "name": "American Coffee",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605194919.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe01fb519f72155ddd05",
+    "_id": "66543b1b8d7af6ceae29d3dd",
+    "__v": 0
+  },
+  {
+    "name": "Latte",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605430883.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe01fb519f72155ddd05",
+    "_id": "66543b1b8d7af6ceae29d3de",
+    "__v": 0
+  },
+  {
+    "name": "Saudi Coffee",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605194339.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe01fb519f72155ddd05",
+    "_id": "66543b1b8d7af6ceae29d3df",
+    "__v": 0
+  },
+  {
+    "name": "Mocha Frappe",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605440013.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe2dfb519f72155ddd07",
+    "_id": "66543b1b8d7af6ceae29d3e0",
+    "__v": 0
+  },
+  {
+    "name": "Long Shot Classic",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605440037.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe2dfb519f72155ddd07",
+    "_id": "66543b1b8d7af6ceae29d3e1",
+    "__v": 0
+  },
+  {
+    "name": "Spanish Latte",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605440066.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afe2dfb519f72155ddd07",
+    "_id": "66543b1b8d7af6ceae29d3e2",
+    "__v": 0
+  },
+  {
+    "name": "Karak Tea",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1612183091.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afec5fb519f72155ddd0a",
+    "_id": "66543b1b8d7af6ceae29d3e3",
+    "__v": 0
+  },
+  {
+    "name": "Adany Tea",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1601826424.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afec5fb519f72155ddd0a",
+    "_id": "66543b1b8d7af6ceae29d3e4",
+    "__v": 0
+  },
+  {
+    "name": "Black tea",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1606217023.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afec5fb519f72155ddd0a",
+    "_id": "66543b1b8d7af6ceae29d3e5",
+    "__v": 0
+  },
+  {
+    "name": "Morocan tea",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1605455164.png",
+    "productAttribute": "664de37f4767bfab8d290a27",
+    "ourMenu": "664afec5fb519f72155ddd0a",
+    "_id": "66543b1b8d7af6ceae29d3e6",
+    "__v": 0
+  },
+  {
+    "name": "Turkish Coffee Premium blend",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671993352.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664afef8fb519f72155ddd0d",
+    "_id": "66543b1b8d7af6ceae29d3e7",
+    "__v": 0
+  },
+  {
+    "name": "Saudi Coffee with cardamom",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671993393.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664afef8fb519f72155ddd0d",
+    "_id": "66543b1b8d7af6ceae29d3e8",
+    "__v": 0
+  },
+  {
+    "name": "Cookies- Dark",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1612268480.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664aff2efb519f72155ddd10",
+    "_id": "66543b1b8d7af6ceae29d3e9",
+    "__v": 0
+  },
+  {
+    "name": "Cookies- White",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1612268530.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664aff2efb519f72155ddd10",
+    "_id": "66543b1b8d7af6ceae29d3ea",
+    "__v": 0
+  },
+  {
+    "name": "Donut- Glazering",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1612268585.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664aff2efb519f72155ddd10",
+    "_id": "66543b1b8d7af6ceae29d3eb",
+    "__v": 0
+  },
+  {
+    "name": "Instant Coffee",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671993663.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b48ee1c70166754067450",
+    "_id": "66543b1b8d7af6ceae29d3ec",
+    "__v": 0
+  },
+  {
+    "name": "Espresso",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671981291.jpg",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49451c70166754067454",
+    "_id": "664b4a321c70166754067467",
+    "__v": 0
+  },
+  {
+    "name": "Cappuccino",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671981337.jpg",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49451c70166754067454",
+    "_id": "66543b1b8d7af6ceae29d3ee",
+    "__v": 0
+  },
+  {
+    "name": "American",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671981386.jpg",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49451c70166754067454",
+    "_id": "66543b1b8d7af6ceae29d3ef",
+    "__v": 0
+  },
+  {
+    "name": "TUMBLER KINTO TRAVEL 500ML STAINLESS",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671980328.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49731c70166754067456",
+    "_id": "66543b1b8d7af6ceae29d3f0",
+    "__v": 0
+  },
+  {
+    "name": "TUMBLER KINTO TRAVEL 500ML WHITE",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671916194.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49731c70166754067456",
+    "_id": "66543b1b8d7af6ceae29d3f1",
+    "__v": 0
+  },
+  {
+    "name": "ZEYU WATER BOTTLE 420ML GOLD",
+    "image": "https://barns.com.sa/public/assets/manage_store/images/product/additional/1671980183.png",
+    "productAttribute": "000000000000000000000000",
+    "ourMenu": "664b49731c70166754067456",
+    "_id": "66543b1b8d7af6ceae29d3f2",
+    "__v": 0
+  }
+]
